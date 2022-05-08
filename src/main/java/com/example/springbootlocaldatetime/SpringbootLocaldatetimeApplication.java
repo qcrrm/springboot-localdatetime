e55047856894fe7b511fbeb7c3511878d361420f
lfchen <lfchen@justhealth.cn>
2022-05-08 16:28:49 +0800

    first commit

---
+++ src/main/java/com/example/springbootlocaldatetime/SpringbootLocaldatetimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlocaldatetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLocaldatetimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLocaldatetimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootlocaldatetime/SpringbootLocaldatetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlocaldatetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLocaldatetimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
